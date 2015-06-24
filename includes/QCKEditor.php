7ff2fc90b1ce486bf4227015f7d45847eb4919a3
spekary <spekary@gmail.com>
2015-06-24 08:29:33 -0700

    Initial implementation

---
+++ includes/QCKEditor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace QCubed\Plugin;
+
+
+class QCKEditor extends QCKEditorBase
+{
+	public function  __construct($objParentObject, $strControlId = null) {
+		parent::__construct($objParentObject, $strControlId);
+	}
+
+}
---
+++ js/QCKSetup.js
@@ -0,0 +1,4 @@
+// CKEditor Setup Functions
+
+	// Must be path to ckeditor relative to DOCROOT
+    var CKEDITOR_BASEPATH = qc.baseDir + "/vendor/ckeditor/ckeditor/";
